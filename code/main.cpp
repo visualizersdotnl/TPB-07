e204467656b4be2b5461a619298d282d7ee52dda
Niels J. de Wit <ndewit@gmail.com>
2019-12-27 01:41:36 +0100

    Tweaked Unity logo (added it to Trevor (TM) shader)

--- code/main.cpp
+++ code/main.cpp
@@ -8,6 +8,7 @@
 	- Cracktro: https://www.shadertoy.com/view/Wl33WS
 	- Caged balls: https://www.shadertoy.com/view/Wt3GWB
 	- Trevor: https://www.shadertoy.com/view/Wtc3DB
+	- Always check for latest before using backups, esp. check different radial blur tweaks
 
 	I swear that I will:
 	- Not overengineer a single bit
