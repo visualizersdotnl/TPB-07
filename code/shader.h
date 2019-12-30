c396f3afea436bafa9c3b9afe2de62e3540b7b9b
Niels J. de Wit <ndewit@gmail.com>
2019-12-30 20:31:16 +0100

    Backup FX looks better (Shadertoy)

--- code/shader.h
+++ code/shader.h
@@ -1,15 +1,9 @@
 
 #pragma once
 
-// we need 3 types:
-// - regular (to (frame)buffer)
-// - to render target #1 (might just be the same as regular)
-// - to cube map #1
-// - a 2D shader for sprites (with fixed path perhaps)
-
 enum ShaderType
 {
 	ST_TO_CUBE,
-	ST_TO_BUFFER
+	ST_TO_BUFFER,
+	ST_TO_2D
 };
-
